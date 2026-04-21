491b330eac9df7bf29b9638768db26d54bde0541
parthbhosale007 <parthbhosale029@gmail.com>
2026-04-21 23:59:34 +0530

    Backend for Repo-method

--- ai-service/app.py
+++ ai-service/app.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from reviewer import analyze_code
 from fastapi.middleware.cors import CORSMiddleware
+from schemas import fetch_repo_code
+
+
+
 
 
 app = FastAPI()
@@ -18,6 +22,9 @@ app.add_middleware(
 class CodeRequest(BaseModel):
     code: str
 
+class RepoRequest(BaseModel):
+    repo_url: str
+
 @app.get("/")
 def home():
     return {"message": "CodeGuard API Running 🚀"}
@@ -25,3 +32,8 @@ def home():
 @app.post("/review")
 def review_code(req: CodeRequest):
     return analyze_code(req.code)
+
+@app.post("/review-repo")
+def review_repo(req: RepoRequest):
+    code = fetch_repo_code(req.repo_url)
+    return analyze_code(code)
