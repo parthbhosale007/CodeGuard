dd6e0f5ff4d4d920b82926aa902f7abb5e8c3627
parthbhosale007 <parthbhosale029@gmail.com>
2026-04-21 23:33:32 +0530

    CORS policy

--- ai-service/app.py
+++ ai-service/app.py
@@ -2,8 +2,18 @@ import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 from reviewer import analyze_code
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For now (we’ll tighten later)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CodeRequest(BaseModel):
     code: str
