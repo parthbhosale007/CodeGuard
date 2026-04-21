1c9d561307ffb44b9ae90d944e865dd3f393dea7
parthbhosale007 <parthbhosale029@gmail.com>
2026-04-22 00:09:32 +0530

    Repo-url fix

--- ai-service/app.py
+++ ai-service/app.py
@@ -3,11 +3,27 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from reviewer import analyze_code
 from fastapi.middleware.cors import CORSMiddleware
-from schemas import fetch_repo_code
+import requests
 
+def fetch_repo_code(repo_url):
+    # Extract owner & repo
+    parts = repo_url.replace("https://github.com/", "").split("/")
+    owner = parts[0]
+    repo = parts[1]
 
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/contents"
 
+    files = requests.get(api_url).json()
 
+    code = ""
+
+    for file in files:
+        if file["type"] == "file":
+            if file["name"].endswith((".py", ".js", ".cpp")):
+                raw = requests.get(file["download_url"]).text
+                code += f"\n\nFile: {file['name']}\n{raw}"
+
+    return code
 
 app = FastAPI()
 
